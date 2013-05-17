480de522e54945e2bb400fe5c85071cce5bdea72
Kenichi Kamiya <kachick1@gmail.com>
2013-05-18 05:43:51 +0900

    Fin 3.2

---
+++ Programming_BjarneStroustrup/3.2.variable.cpp
@@ -0,0 +1,12 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+  string name = "Annemarie";
+  int number_of_steps = 39;
+  // string name2 = 39; // こういうのはエラー
+  // int number_of_steps2 = "Annnemarie"; // こういうのはエラー
+  double flying_times = 3.5;
+  char decimal_pointer = '.'; // 1文字であること。Rubyのシングルクォートとかとは違う
+  bool tap_on = true; // 他にはfalseのみ
+}
