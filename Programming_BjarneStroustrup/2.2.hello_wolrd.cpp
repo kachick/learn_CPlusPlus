06a5eee6f2c9882c923f00c7d1c3b20cc03498d7
Kenichi Kamiya <kachick1@gmail.com>
2013-05-17 23:55:31 +0900

    2.2

---
+++ Programming_BjarneStroustrup/2.2.hello_wolrd.cpp
@@ -0,0 +1,9 @@
+// This program displays "Hello, world" to the monitor :)
+
+#include "std_lib_facilities.h"
+
+int main() // C系ならmainは必須やで
+{
+	cout << "Hello, World!\n"; // cout は "c"haracter "out"put stream の略なんだと。
+	return 0;
+}
